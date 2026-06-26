7f9df23d4b9e130ed1b76673a67b6217d3cf1942
Ansh Tyagi <officialanshtyagi112@gmail.com>
2026-06-26 20:03:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+import Navbar from "./components/Navbar";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <Navbar />
+      </main>
+    </>
+  )
+}
+
+export default App
