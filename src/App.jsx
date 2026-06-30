f3e3de6e1c20cc1ff5e931ce7f9c1afd4d9ebfaf
Ansh Tyagi <officialanshtyagi112@gmail.com>
2026-06-30 23:59:19 +0530

    Add Menu and Contact components; enhance Art component with GSAP animations

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
+import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -18,7 +20,9 @@ function App() {
         <Hero />
         <Cocktails />
         <About />
-        <Art/>
+        <Art />
+        <Menu />
+        <Contact />
       </main>
     </>
   )
